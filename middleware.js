9ea5a9a5e56b79a5614141756c5b9601c2ea07c8
brandonottaway <brandonottaway@gmail.com>
2026-02-26 14:26:39 -0800

    new file:   middleware.js

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  if (request.nextUrl.pathname.startsWith('/epk')) {
+    const basicAuth = request.headers.get('authorization');
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
+
+      if (user === 'hallway' && pwd === 'scenes2026') {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse('Authentication required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
